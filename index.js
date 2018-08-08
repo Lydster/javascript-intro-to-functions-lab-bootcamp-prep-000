ec6b570756a05cc9b12f2fba4a21a3deffa4aabd
Lydia Thornton <lydia.d.thornton@gmail.com>
2018-08-08 01:21:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === lowercase)
   return "I can't hear you!";
   else if (string.toUpperCase() === uppercase)
-  return "YES INDEED";
+  return "YES INDEED!";
   else if (string === mixedCase)
   return "I love you, too.";
   else
   return "test failed";
 }
   
-sayHiToGrandma('HELLO!')
-sayHiToGrandma('hello!')
-sayHiToGrandma("I love you, Grandma.")
+sayHiToGrandma('HELLO!');
+sayHiToGrandma('hello!');
+sayHiToGrandma("I love you, Grandma.");
